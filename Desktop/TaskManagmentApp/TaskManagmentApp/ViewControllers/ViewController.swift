5f74315028a17c8a815635f13fcae386c8ab6c21
Agladze-Tekla <teklagladze@gmail.com>
2024-01-23 14:22:00 -0800

    First commit

---
+++ Desktop/TaskManagmentApp/TaskManagmentApp/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagmentApp
+//
+//  Created by Tekla on 1/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemTeal
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
