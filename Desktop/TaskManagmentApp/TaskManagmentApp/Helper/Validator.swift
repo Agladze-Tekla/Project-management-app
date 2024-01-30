5530f679553e0fde218ad26d170cc1221f209141
Agladze-Tekla <teklagladze@gmail.com>
2024-01-30 08:24:19 -0800

    Added Validator class

---
+++ Desktop/TaskManagmentApp/TaskManagmentApp/Helper/Validator.swift
@@ -0,0 +1,31 @@
+//
+//  Validator.swift
+//  TaskManagmentApp
+//
+//  Created by Tekla on 1/30/24.
+//
+
+import UIKit
+
+final class Validator {
+    static func isValidEmail(email: String) -> Bool {
+        let email = email.trimmingCharacters(in: .whitespacesAndNewlines)
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.{1}[A-Za-z]{2,64}"
+        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    static func isValidUsername(username: String) -> Bool {
+        let username = username.trimmingCharacters(in: .whitespacesAndNewlines)
+        let usernameRegEx = "\\w{4,24}"
+        let usernamePred = NSPredicate(format: "SELF MATHCES %@", usernameRegEx)
+        return usernamePred.evaluate(with: username)
+    }
+    
+    static func isPasswordValid(password: String) -> Bool {
+        let password = password.trimmingCharacters(in: .whitespacesAndNewlines)
+        let passwordRegEx = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[$@$#!%?&]).{6,32}$"
+        let passwordPred = NSPredicate(format: "SELF MATHCES %@", passwordRegEx)
+        return passwordPred.evaluate(with: password)
+    }
+}
