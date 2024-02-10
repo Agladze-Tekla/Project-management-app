9bda13a739c72d5cf541dece9082eb90e76e1246
Agladze-Tekla <teklagladze@gmail.com>
2024-02-10 03:20:47 -0800

    Added EditItemFolder

---
+++ Desktop/TaskManagmentApp/TaskManagmentApp/ViewControllers/EditItemViews/EditTaskViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EditTaskViewController.swift
+//  TaskManagmentApp
+//
+//  Created by Tekla on 2/10/24.
+//
+
+import UIKit
+
+final class EditTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
