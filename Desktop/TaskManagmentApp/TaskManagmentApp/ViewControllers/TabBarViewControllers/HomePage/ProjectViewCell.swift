8fbffc72ae52e1a137a34d9d7a914a0332a0724c
Agladze-Tekla <teklagladze@gmail.com>
2024-02-10 16:43:37 -0800

    simple project progressBar

--- Desktop/TaskManagmentApp/TaskManagmentApp/ViewControllers/TabBarViewControllers/HomePage/ProjectViewCell.swift
+++ Desktop/TaskManagmentApp/TaskManagmentApp/ViewControllers/TabBarViewControllers/HomePage/ProjectViewCell.swift
@@ -15,17 +15,39 @@ final class ProjectViewCell: UICollectionViewCell {
            let label = UILabel()
            label.font = .boldSystemFont(ofSize: 20)
            label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
            label.textColor = .white
            return label
        }()
+    
+    private var currentProject: ProjectModel?
+    
+    private var progressBar: UIProgressView = {
+        let bar = UIProgressView(progressViewStyle: .default)
+        bar.backgroundColor = .purple//UIColor.systemIndigo.withAlphaComponent(0.3)
+        bar.progressTintColor = .white
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        return bar
+    }()
        
+    private let cellStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.alignment = .leading
+        stack.spacing = 10
+        stack.distribution = .equalSpacing
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    
     //MARK: - Init
       override init(frame: CGRect) {
         super.init(frame: frame)
         setupBackground()
         addSubViews()
         setUpConstraints()
+        setupProgressBar()
       }
       
       required init?(coder: NSCoder) {
@@ -40,20 +62,32 @@ final class ProjectViewCell: UICollectionViewCell {
     }
     
     private func addSubViews() {
-        contentView.addSubview(titleLabel)
+        cellStackView.addArrangedSubview(titleLabel)
+        cellStackView.addArrangedSubview(progressBar)
+        contentView.addSubview(cellStackView)
         }
     
     private func setUpConstraints() {
         NSLayoutConstraint.activate([
-                  titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
-                  titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
-                  titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
-                  titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-              ])
+            progressBar.leadingAnchor.constraint(equalTo: cellStackView.leadingAnchor, constant: 20),
+            progressBar.trailingAnchor.constraint(equalTo: cellStackView.trailingAnchor, constant: -20),
+            cellStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            cellStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            cellStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
        }
+    
+    private func setupProgressBar() {
+        let completedTasks = 13
+        let allTasks = 19
+        let progress = Float(completedTasks)/Float(allTasks)
+        progressBar.setProgress(progress, animated: true)
+    }
     
     //MARK: - Configure
     func configurate(project: ProjectModel) {
+        currentProject = project
         titleLabel.text = project.title
         }
     
